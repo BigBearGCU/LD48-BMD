173b94c31138c75433ab929d215a05f8fdfbd33f
Brian McDonald <brian.mcdonald1980@googlemail.com>
2014-04-26 16:00:10 +0100

    Initial Mechanics in

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GunScript : MonoBehaviour {
+
+	public GameObject targetPixel;
+	public GridScript grid;
+
+	public int targetPixelRow=0;
+	public int targetPixelColumn=0;
+
+	public MagnetScript topMagnet;
+	public MagnetScript leftMagnet;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CheckFire",0.1f,2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SetTarget(int row,int column)
+	{
+		GameObject obj=grid.GetPixel(row,column);
+		if (obj!=null)
+			targetPixel.transform.position=obj.transform.position;
+	}
+
+	void CheckFire()
+	{
+		//TODO: Want it to march down all the time
+		targetPixelRow+=topMagnet.currentForce;
+		targetPixelColumn+=leftMagnet.currentForce;
+		SetTarget(targetPixelRow,targetPixelColumn);
+	}
+}
---
+++ Assets/Scripts/MagnetScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetScript : MonoBehaviour {
+
+	public int currentForce=0;
+	public int maxForce=10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown(gameObject.name+"Pos"))
+		{
+			currentForce+=1;
+		}
+		else if(Input.GetButtonDown(gameObject.name+"Neg"))
+		{
+			currentForce-=1;
+		}
+	}
+
+	void OnGUI()
+	{
+		//percentage, position
+	}
+}
